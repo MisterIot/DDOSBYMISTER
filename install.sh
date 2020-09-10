f283d92ef22e42526eab0cbd862b0220501c9406
MisterIot <70957360+MisterIot@users.noreply.github.com>
2020-09-10 11:55:36 -0300

    setup.py

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-# DDOSBYMISTER
+from distutils.core import setup
+
+setup(
+    name="Slowloris",
+    py_modules=["slowloris"],
+    entry_points={"console_scripts": ["slowloris=slowloris:main"]},
+    version="0.2.1",
+    description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
+    author="Mister ",
+    author_email="procurandouqfdp@gmail.com",
+    url="urlaq",
+    keywords=["dos", "http", "slowloris"],
+    license="MIT",
